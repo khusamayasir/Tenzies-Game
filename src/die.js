13a9530fed9f1e044c2d5f39c9834e46833a1645
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-31 11:42:34 +0500

    Update Die.js

--- src/die.js
+++ src/die.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from "react"
 
-export default function die() {
+export default function Die() {
+
   return (
-    <div>die</div>
+      <div>...</div>
   )
 }
