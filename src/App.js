9b0e20c1bc54df8129a39db7b51ef0c61e28bb42
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-31 23:49:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from "react"
 export default function App() {
 
   return (
-    <div>
+    <main>
         <h1>Tenzies</h1>
         <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <button>
             Roll
         </button>
-    </div>
+    </main>
   )
 }
