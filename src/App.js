64d03d001cacadf17e478fa6f7037383e68c090f
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-31 23:53:44 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <main>
         <h1>Tenzies</h1>
         <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-        <button>
+        <button className="roll-dice">
             Roll
         </button>
     </main>
