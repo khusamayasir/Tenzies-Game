38006c904f3936a1fd2a29ffb48874e75c145ee0
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-02 10:18:38 +0500

    Add Confetti App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Die from "./Die"
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
 
@@ -65,6 +66,7 @@ export default function App() {
 
   return (
     <main>
+        {tenzies && <Confetti />}
         <h1 className="titile">Tenzies</h1>
         <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div claasName="dice-container">
