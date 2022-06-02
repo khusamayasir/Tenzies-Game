a77cf9b0fe07db9c46e9497fe6f7fa4b1888c80a
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-06-03 01:07:11 +0500

    Update Darkmode.js Add btn

--- src/components/Darkmode.js
+++ src/components/Darkmode.js
@@ -3,7 +3,9 @@ import React from "react"
 export default function DarkMode() {
   return (
     <div className="dark">
-
+		<button className="btn" >
+			Toggle Mode
+		</button>
     </div>
   )
 }
